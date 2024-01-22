45fc44ac4097833fd975587dcce8cc84bdc4e9c7
Alexander2049 <a.alexander.2049@gmail.com>
2024-01-22 15:26:44 +0200

    installed express js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-console.log("Application is running :)");
-console.log("Edit ./src/index.ts to start working");
+import express from "express";
+
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
