2849f09dd33bc26912774e574fc4c1a51888b710
Alexander2049 <a.alexander.2049@gmail.com>
2024-01-22 22:36:47 +0200

    moved mongo db connection to main

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import express from "express";
-import runDb from "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 80;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const DB_URI: string | undefined = process.env.DB_URI;
+if (!DB_URI) throw new Error("DB_URI is required");
+
+const mongoClient = new MongoClient(DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await mongoClient.connect();
+    // Send a ping to confirm a successful connection
+    await mongoClient.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-runDb().catch(console.dir);
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log("main() error: " + error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoClient.close();
+  }
+}
+
+main().catch(console.dir);
