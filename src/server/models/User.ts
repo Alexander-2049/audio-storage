c029962d54055e46358add68c4a6e247c19cdee1
Alexander-2049 <a.alexander.2049@gmail.com>
2024-01-25 01:12:51 +0200

    name refactoring

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  ip: { type: String, required: true },
+  reg_date: { type: Date, default: Date.now },
+  reg_ip: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
