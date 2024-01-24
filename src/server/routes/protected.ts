5782880847c8551027c17a2521bdce2727f3b81f
Alexander-2049 <a.alexander.2049@gmail.com>
2024-01-25 01:13:05 +0200

    actions for testing purposes

--- src/server/routes/protected.ts
+++ src/server/routes/protected.ts
@@ -3,8 +3,13 @@ import verifyToken from "../middleware/authMiddleware";
 
 const protectedRouter = express.Router();
 
-protectedRouter.get("/", verifyToken, (req: Request, res: Response) => {
-  res.status(200).json({ message: "Protected route accessed" });
+protectedRouter.get("/", verifyToken, async (req: Request, res: Response) => {
+  if (res.locals.userId) {
+    const user = res.locals.user;
+    return res.status(200).json({ message: "Route accessed by " + user?.username });
+  }
+
+  return res.status(200).json({ message: "Protected route accessed" });
 });
 
 export default protectedRouter;
