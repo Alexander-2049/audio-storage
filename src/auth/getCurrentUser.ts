ae97177c04b4539ab4bea8fe24d470b404d7428c
Alexander-2049 <a.alexander.2049@gmail.com>
2024-02-04 16:11:40 +0200

    feature: get user data function

---
+++ src/auth/getCurrentUser.ts
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import User from "@/models/User";
+
+export default async function getCurrentUser() {
+  const token = cookies().get("token");
+  if (typeof token?.value !== "string") return null;
+
+  const SECRET_TOKEN = process.env.SECRET_TOKEN;
+  if (typeof SECRET_TOKEN !== "string") return null;
+
+  try {
+    const verifiedData = jwt.verify(token.value, SECRET_TOKEN) as JwtPayload;
+    if (!verifiedData) return null;
+
+    const user = await User.findById(verifiedData.userId);
+    
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
