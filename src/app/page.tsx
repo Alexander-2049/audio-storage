99a32b8c02c18dcaf6d967d83e6cdef0cdac0a17
Alexander-2049 <a.alexander.2049@gmail.com>
2024-02-04 17:37:18 +0200

    added Player to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-export default function Home() {
+import getCurrentUser from "@/auth/getCurrentUser";
+import Player from "@/components/Player/Player";
+
+export default async function Home() {
+  const user = await getCurrentUser();
+  
   return (
     <main>
+      <Player user={user}/>
     </main>
   );
 }
