cc9b0a5494e81bdc4b1fb7a3db63007c80e27608
Alexander2049 <a.alexander.2049@gmail.com>
2024-02-06 13:55:36 +0200

    small fix

--- src/components/AudioPlayer/PlayerContent.tsx
+++ src/components/AudioPlayer/PlayerContent.tsx
@@ -4,10 +4,6 @@ export default function PlayerContent({ children }: { children: ReactNode }) {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-auto">
       {children}
-      {children}
-      {children}
-      {children}
-      {children}
     </main>
   );
 }
