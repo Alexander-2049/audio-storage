3187f3a97e9e3308fcc2ec096e3b2695b997bfec
Alexander-2049 <a.alexander.2049@gmail.com>
2024-02-07 22:57:41 +0200

    Added boilerplate pages

---
+++ src/app/library/page.tsx
@@ -0,0 +1,9 @@
+const LibraryPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h2>Library</h2>
+    </div>
+  );
+};
+
+export default LibraryPage;
---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+const SearchPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h2>Search</h2>
+    </div>
+  );
+};
+
+export default SearchPage;
