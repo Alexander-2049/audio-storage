fba3859b58170a84b633b97133ae8f5d3e669f89
Alexander-2049 <a.alexander.2049@gmail.com>
2024-02-07 22:56:00 +0200

    Separated Top Nav Buttons from main component

--- src/components/AudioPlayer/PlayerNav.tsx
+++ src/components/AudioPlayer/PlayerNav.tsx
@@ -1,15 +1,10 @@
 import Link from "next/link";
-import {
-  BellIcon,
-  HomeIcon,
-  LibraryIcon,
-  MusicIcon,
-  SearchIcon,
-} from "./PlayerIcons";
+import { BellIcon, MusicIcon } from "./PlayerIcons";
 import { Button } from "../ui/button";
 import { IUser } from "@/models/User";
+import PlayerNavTopButtons from "./PlayerNavTopButtons";
 
-export default function NavigationMenu({user}: {user: IUser | null}) {
+export default function NavigationMenu({ user }: { user: IUser | null }) {
   return (
     <nav className="hidden items-start text-sm font-medium lg:grid">
       <div className="h-full border-r bg-gray-100/40 dark:bg-gray-800/40">
@@ -26,27 +21,7 @@ export default function NavigationMenu({user}: {user: IUser | null}) {
           </div>
           <div className="flex-1 overflow-auto py-2">
             <nav className="grid items-start px-4 text-sm font-medium">
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="/"
-              >
-                <HomeIcon className="h-4 w-4" />
-                Home
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <SearchIcon className="h-4 w-4" />
-                Search
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-                href="#"
-              >
-                <LibraryIcon className="h-4 w-4" />
-                Your Library
-              </Link>
+              <PlayerNavTopButtons />
               <h2 className="mt-4 mb-2 text-gray-900 dark:text-gray-50">
                 Playlists
               </h2>
