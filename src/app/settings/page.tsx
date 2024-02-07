e2e0b5bba5fde443ff9335f0d09b091853ebbacd
Alexander-2049 <a.alexander.2049@gmail.com>
2024-02-07 22:34:02 +0200

    Added boilerplate settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h2>Settings</h2>
+    </div>
+  );
+};
+
+export default SettingsPage;
