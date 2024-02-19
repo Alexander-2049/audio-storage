a44b29a648145849f76cc58b360887dc92249e24
Alexander2049 <a.alexander.2049@gmail.com>
2024-02-19 14:44:45 +0200

    Updated: Added chunks route

--- src/server/routes/music/index.ts
+++ src/server/routes/music/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import musicSearchRouter from "./search";
 import musicInfoRouter from "./info";
+import musicChunksRouter from "./chunks";
 
 const musicRouter = Router();
 
 musicRouter.use("/search", musicSearchRouter);
 musicRouter.use("/info", musicInfoRouter);
+musicRouter.use("/chunks", musicChunksRouter);
 
 export default musicRouter;
