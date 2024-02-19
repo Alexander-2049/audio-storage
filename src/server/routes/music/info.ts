06f32ee6c3c29fbb94563132beb418bf3c0ad71c
Alexander2049 <a.alexander.2049@gmail.com>
2024-02-19 14:45:14 +0200

    Updated: Implemented basic logic for POST:/api/music/info

--- src/server/routes/music/info.ts
+++ src/server/routes/music/info.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import Z3 from "../../models/Z3";
+import filestorage from "../../models/FileStorage";
+import Song_DB from "../../database/audio/Song";
 
 const musicInfoRouter = Router();
 
@@ -9,7 +11,34 @@ musicInfoRouter.post("/", async (req, res) => {
 
   const id = req.body.id;
 
-  return res.json(await Z3.getSongInfo(id));
+  const info = await Z3.getSongInfo(id);
+  if (!info) return res.status(404).json({ error: "Song not found" });
+
+  const { song_id, file_size } = info;
+  res.json(info);
+
+  // TODO: Check the logic here
+  // File size has to be taken from source and then saved to DB
+  // and then after that should be checked if filesize is similar
+  // to size that is stored in DB
+  if (filestorage.existsSync(song_id) && !file_size) {
+    const file_size = filestorage.sizeSync(song_id);
+    await Song_DB.findOneAndUpdate({ song_id }, { file_size });
+    return;
+  } else if (filestorage.existsSync(song_id)) return;
+
+  if (song_id.startsWith("z3_")) {
+    Z3.fetchSourceURL(song_id.slice(3))
+      .then((source_url) => {
+        if (source_url)
+          Z3.downloadSong(source_url, song_id).then(({ file_size }) => {
+            Song_DB.findOneAndUpdate({ song_id }, { file_size });
+          });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 });
 
 export default musicInfoRouter;
