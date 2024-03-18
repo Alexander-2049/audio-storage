cb5db2043a93ed0c05aa92226cf544ec2245f760
Alexander2049 <a.alexander.2049@gmail.com>
2024-03-18 09:45:08 +0200

    Connected /search page to search API

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,18 +1,57 @@
 "use client";
+import API from "@/utils/API";
+import {
+  QueryFunctionContext,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 // import { PlayerContext } from "@/components/AudioPlayer/context/playerContext";
 import { /* useRouter, */ useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 // import { useContext } from "react";
+
+async function testFunction(value: QueryFunctionContext) {
+  const query = value.queryKey[1] || "";
+  if (typeof query !== "string") return [];
+  if (query.length <= 2) return [];
+
+  const response = await API.POST("/api/music/search", {
+    query,
+  });
+
+  const decoded_data = await response.json();
+
+  return decoded_data;
+}
 
 const SearchPage = () => {
   // const { song_id, setSongId } = useContext(PlayerContext);
   const searchParams = useSearchParams();
+  // Access the client
+  const queryClient = useQueryClient();
+  const [searchKeywords, setSearchKeywords] = useState("");
+
+  // Queries
+  const query = useQuery({
+    queryKey: ["search_query", searchKeywords],
+    queryFn: testFunction,
+  });
   // const router = useRouter();
+  useEffect(() => {
+    setSearchKeywords(searchParams.get("q") || "");
+  }, [searchParams]);
 
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col">
-      {searchParams.get("q") ? (
+    <div className="w-full h-full">
+      {searchParams.get("q") && !query.isFetched ? (
         <h2>Searching for {searchParams.get("q")}...</h2>
       ) : null}
+      <ul>
+        {Array.isArray(query.data) &&
+          query.data.map((song) => {
+            return <li key={`search_${song.song_id}`}>{song.song_name}</li>;
+          })}
+      </ul>
     </div>
   );
 };
--- src/components/AudioPlayer/PlayerContent.tsx
+++ src/components/AudioPlayer/PlayerContent.tsx
@@ -1,14 +1,15 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
-export default function PlayerContent({
-  children,
-}: {
-  children: ReactNode;
-}) {
+const queryClient = new QueryClient();
+
+export default function PlayerContent({ children }: { children: ReactNode }) {
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-auto">
-      {children}
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-auto">
+        {children}
+      </main>
+    </QueryClientProvider>
   );
 }
